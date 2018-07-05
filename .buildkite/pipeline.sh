f3c8edb233a838da5b689166f27c476cdc9c1762
tobjaw <tobjaw@gmail.com>
2018-07-05 23:59:51 +0200

    chore(ci): use output file instead of stdout
    due to limitations of in-place-editing of jq,
    just using the stdout sadly does not work

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -16,14 +16,14 @@ set -euo pipefail
 #   fall back to rebuilding all images.
 #
 # USAGE:
-#   .buildkite/pipeline.sh | buildkite-agent pipeline upload
+#   .buildkite/pipeline.sh && buildkite-agent pipeline upload
 #
 ###########################
 
 ROOT_PIPELINE_CONFIG=.buildkite/pipeline.config.json
 export IMAGE_PIPELINE_CONFIG=pipeline.config.json
 export IMAGES_ENABLED=/tmp/images_enabled
-export PIPELINE_FINAL=/tmp/pipeline_final
+export PIPELINE_OUT=.buildkite/pipeline.json
 export STEPS_DIR=.buildkite/steps
 
 # diff current commit to last
@@ -46,9 +46,9 @@ imagesList() {
 imageEnable() {
     IMAGE_ID="$1"
     if [ ! -f "${IMAGES_DIR}/${IMAGE_ID}/${IMAGE_PIPELINE_CONFIG}" ]; then
-        printf "%-40s \e[31m⚠ need rebuild - config missing\e[39m\n" $1 >&2
+        printf "%-40s \e[31m⚠ need rebuild - config missing\e[39m\n" $1
     else
-        printf "%-40s \e[33m✖ need rebuild\e[39m\n" $1 >&2
+        printf "%-40s \e[33m✖ need rebuild\e[39m\n" $1
         echo "${IMAGE_ID}" >> ${IMAGES_ENABLED}
     fi
 }
@@ -62,7 +62,7 @@ imageEnableMaybe() {
         # commit or image is dirty, try to rebuild
         imageEnable $1
     else
-        printf "%-40s \e[32m✓ up to date\e[39m\n" ${IMAGE_ID} >&2
+        printf "%-40s \e[32m✓ up to date\e[39m\n" ${IMAGE_ID}
     fi
 }
 export -f imageEnableMaybe
@@ -82,33 +82,33 @@ pipelineEnableAll() {
 main() {
     # init temporary files
     rm ${IMAGES_ENABLED} 2> /dev/null || true
-    rm ${PIPELINE_FINAL} 2> /dev/null || true
-    touch ${PIPELINE_FINAL}
+    rm ${PIPELINE_OUT} 2> /dev/null || true
+    touch ${PIPELINE_OUT}
 
     # enable images if needed
-    echo "+++ Checking images" >&2
+    echo "+++ Checking images"
     if commitIsDirty; then
-        printf "\e[33mcommit is dirty, enabling pipeline for all images\e[39m\n" >&2
+        printf "\e[33mcommit is dirty, enabling pipeline for all images\e[39m\n"
         pipelineEnableAll 1
     else
-        printf "\e[32mcommit is clean, enabling pipeline for changed images only\e[39m\n" >&2
+        printf "\e[32mcommit is clean, enabling pipeline for changed images only\e[39m\n"
         pipelineEnableAll 0
     fi
 
     # iterate over build groups (e.g. build, test, ...)
     for GROUP in $(jq -r '.groups[].id' ${ROOT_PIPELINE_CONFIG}); do
-        echo -e "\n+++ $GROUP group" >& 2
+        echo -e "\n+++ Group $GROUP"
         # iterate over images that need to be rebuilt
         while read IMAGE_NAME; do
             # get step from image pipeline config
             STEP=$(jq -r ".steps.${GROUP}" ${IMAGES_DIR}/${IMAGE_NAME}/${IMAGE_PIPELINE_CONFIG})
             if [ "$STEP" == "null" ]; then
-                printf "%-40s \e[33m✖ no step defined\e[39m\n" ${IMAGE_NAME}>&2
+                printf "%-40s \e[33m✖ no step defined\e[39m\n" ${IMAGE_NAME}
                 continue
             fi
 
             if [ ! -f ${STEPS_DIR}/${STEP}.json ]; then
-                printf "%-40s \e[31m⚠ step %s does not exist\e[39m\n" ${IMAGE_NAME} ${STEP} >&2
+                printf "%-40s \e[31m⚠ step %s does not exist\e[39m\n" ${IMAGE_NAME} ${STEP}
                 exit 1
             fi
 
@@ -116,14 +116,13 @@ main() {
             jq -n \
                 --arg image_name "${IMAGE_NAME}" \
                 --slurpfile steps "${STEPS_DIR}/${STEP}.json" \
-                '$steps[] | map(.env.IMAGE_NAME=$image_name | .name=$image_name + " " + .name) as $steps | {"steps": $steps }' ${PIPELINE_FINAL}
-
-            printf "%-40s \e[32m✓ using step %s\e[39m\n" ${IMAGE_NAME} ${STEP} >&2
+                '$steps[] | map(.env.IMAGE_NAME=$image_name | .name=$image_name + " " + .name) as $steps | {"steps": $steps }' > ${PIPELINE_OUT}
+            printf "%-40s \e[32m✓ using step '%s'\e[39m\n" ${IMAGE_NAME} ${STEP}
         done <${IMAGES_ENABLED}
     done
 
-    # pipe final config to buildkite-agent upload
-    cat ${PIPELINE_FINAL}
+    echo -e "\n--- Config built"
+    jq '.' ${PIPELINE_OUT}
 }
 
 main
