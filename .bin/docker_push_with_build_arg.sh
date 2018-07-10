967b95f989afabf674ae39955a426d59250fb60e
tobjaw <tobjaw@gmail.com>
2018-07-10 15:02:50 +0200

    feat(ci): docker push use alias for variants

--- .bin/docker_push_with_build_arg.sh
+++ .bin/docker_push_with_build_arg.sh
@@ -7,5 +7,9 @@ IMAGES_DIR_FULL="${IMAGES_DIR}/${IMAGE_NAME}"
 # split `BUILD_ARG_VARIANTS` by `;`
 IFS=';' read -ra VARIANTS <<< "${BUILD_ARG_VARIANTS}"
 for VARIANT in "${VARIANTS[@]}"; do
+    # push unique image
     docker push gridx/${IMAGE_NAME}:${TAG}-${VARIANT}
+
+    # push alias
+    docker push gridx/${IMAGE_NAME}:${VARIANT}
 done
