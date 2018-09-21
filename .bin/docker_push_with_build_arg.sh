5709371112e324d07eb5f6f255e88c32d308a3ee
ecktom <t.eck@gridx.de>
2018-09-21 16:02:45 +0200

    fix(ci): Fix docker image names

--- .bin/docker_push_with_build_arg.sh
+++ .bin/docker_push_with_build_arg.sh
@@ -7,7 +7,7 @@ SUFFIX=${IMAGE_NAME_SUFFIX:-}
 
 if [ ! -z ${TARGET_ARCH:-} ]; then 
     TAG=${TAG}-${TARGET_ARCH}
-    TARGET_ARCH=${TARGET_ARCH}/
+    TARGET_ARCH=-${TARGET_ARCH}
 fi
 
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
@@ -16,8 +16,8 @@ docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 IFS=';' read -ra VARIANTS <<< "${BUILD_ARG_VARIANTS}"
 for VARIANT in "${VARIANTS[@]}"; do
     # push unique image
-    docker push gridx/${TARGET_ARCH:-}${IMAGE_NAME}${SUFFIX}:${TAG}-${VARIANT}
+    docker push gridx/${IMAGE_NAME}${SUFFIX}${TARGET_ARCH:-}:${TAG}-${VARIANT}
 
     # push alias
-    docker push gridx/${TARGET_ARCH:-}${IMAGE_NAME}${SUFFIX}:${VARIANT}
+    docker push gridx/${IMAGE_NAME}${SUFFIX}${TARGET_ARCH:-}:${VARIANT}
 done
