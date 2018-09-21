0e2cd04f20138a8196d6cfb6844082cdf337df0c
ecktom <t.eck@gridx.de>
2018-09-21 10:23:46 +0200

    feat(multiarch): Choosing correct multiarch golang image

--- .bin/docker_build_with_build_arg.sh
+++ .bin/docker_build_with_build_arg.sh
@@ -4,16 +4,23 @@ set -eou pipefail
 TAG=$(.bin/git_tag.sh)
 IMAGES_DIR_FULL="${IMAGES_DIR}/${IMAGE_NAME}"
 SUFFIX=${IMAGE_NAME_SUFFIX:-}
+DOCKERFILE=Dockerfile
+
+if [ ! -z ${TARGET_ARCH:-} ]; then 
+    TAG=${TAG}-${TARGET_ARCH}
+    DOCKERFILE=Dockerfile.${TARGET_ARCH}
+    TARGET_ARCH=${TARGET_ARCH}/
+fi
 
 # split `BUILD_ARG_VARIANTS` by `;`
 IFS=';' read -ra VARIANTS <<< "${BUILD_ARG_VARIANTS}"
 for VARIANT in "${VARIANTS[@]}"; do
     # build unique image
-    docker build -f ${IMAGES_DIR_FULL}/Dockerfile \
-           -t gridx/${IMAGE_NAME}${SUFFIX}:${TAG}-${VARIANT} \
+    docker build -f ${IMAGES_DIR_FULL}/${DOCKERFILE} \
+           -t gridx/${TARGET_ARCH:-}${IMAGE_NAME}${SUFFIX}:${TAG}-${VARIANT} \
            --build-arg ${BUILD_ARG}=${VARIANT} \
            ${IMAGES_DIR_FULL}
 
     # tag alias
-    docker tag gridx/${IMAGE_NAME}${SUFFIX}:${TAG}-${VARIANT} gridx/${IMAGE_NAME}${SUFFIX}:${VARIANT}
+    docker tag gridx/${TARGET_ARCH:-}${IMAGE_NAME}${SUFFIX}:${TAG}-${VARIANT} gridx/${TARGET_ARCH:-}${IMAGE_NAME}${SUFFIX}:${VARIANT}
 done
--- .bin/docker_push.sh
+++ .bin/docker_push.sh
@@ -4,8 +4,14 @@ set -eou pipefail
 TAG=$(.bin/git_tag.sh)
 SUFFIX=${IMAGE_NAME_SUFFIX:-}
 
+if [ ! -z ${TARGET_ARCH:-} ]; then 
+    TAG=${TAG}-${TARGET_ARCH}
+    TARGET_ARCH=${TARGET_ARCH}/
+fi
+
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-docker push gridx/${IMAGE_NAME}${SUFFIX}:$TAG
+docker push gridx/${TARGET_ARCH:-}${IMAGE_NAME}${SUFFIX}:$TAG
+
 
 # push alias
-docker push gridx/${IMAGE_NAME}${SUFFIX}
+docker push gridx/${TARGET_ARCH:-}${IMAGE_NAME}${SUFFIX}
